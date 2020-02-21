c1f90790f7013cc004047a4373ef321b36a1bfb6
meddion <volodia23byk@gmail.com>
2020-02-21 17:24:25 +0200

    Updates

---
+++ precache-manifest.c4ebe664c7452b0a908028eb113a1206.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4deaef5f9b6eb20a523ef04d527b90",
+    "url": "/meddion/meddion.github.io/index.html"
+  },
+  {
+    "revision": "3c12795ecc9b9de4db75",
+    "url": "/meddion/meddion.github.io/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "e5ca74feda7d14a12936",
+    "url": "/meddion/meddion.github.io/static/css/main.51b515db.chunk.css"
+  },
+  {
+    "revision": "3c12795ecc9b9de4db75",
+    "url": "/meddion/meddion.github.io/static/js/2.af9c56a2.chunk.js"
+  },
+  {
+    "revision": "edd029e0ab8ffe06ae330ade938e312c",
+    "url": "/meddion/meddion.github.io/static/js/2.af9c56a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5ca74feda7d14a12936",
+    "url": "/meddion/meddion.github.io/static/js/main.184caf35.chunk.js"
+  },
+  {
+    "revision": "22ed5ec338cb0fa4db8b",
+    "url": "/meddion/meddion.github.io/static/js/runtime-main.02c3c311.js"
+  }
+]);
