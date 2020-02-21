6a4e97a73e951f9aff892b31910fe5671b07f161
meddion <volodia23byk@gmail.com>
2020-02-21 17:52:27 +0200

    Updates

---
+++ precache-manifest.2d7b6cadeacc29ea3d0079b887f5174e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a05c971b17ba9fe31730a505ac8b9c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c12795ecc9b9de4db75",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "44afb0423dc75d515820",
+    "url": "/static/css/main.51b515db.chunk.css"
+  },
+  {
+    "revision": "3c12795ecc9b9de4db75",
+    "url": "/static/js/2.af9c56a2.chunk.js"
+  },
+  {
+    "revision": "edd029e0ab8ffe06ae330ade938e312c",
+    "url": "/static/js/2.af9c56a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44afb0423dc75d515820",
+    "url": "/static/js/main.d98f87d7.chunk.js"
+  },
+  {
+    "revision": "2eb5ea446c722acffd56",
+    "url": "/static/js/runtime-main.bb040c98.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meddion/meddion.github.io/precache-manifest.c4ebe664c7452b0a908028eb113a1206.js"
+  "/precache-manifest.2d7b6cadeacc29ea3d0079b887f5174e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meddion/meddion.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
